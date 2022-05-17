23cdc4733e3aa9c607f37a24b2d6c699cd01cd3d
olususi oluyemi <yemiwebby@gmail.com>
2022-05-17 14:01:20 +0100

    Completed the review

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h1>Home</h1>
-    <p>This is Home page Deployed to Netlify</p>
-    <nuxt-link to="/about">Navigate to About Page </nuxt-link>
+    <p>This is Home page deployed to Netlify</p>
+    <nuxt-link to="/about"> About Page </nuxt-link>
   </div>
 </template>
