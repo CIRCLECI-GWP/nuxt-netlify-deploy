d8b79953b3328c9f2c132ed9743644c7aef13b6e
olususi oluyemi <yemiwebby@gmail.com>
2022-05-17 12:18:46 +0100

    fix

--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,6 @@
   <div>
     <h1>Home</h1>
     <p>This is Home page Deployed to Netlify</p>
-    <nuxt-link to="/about">Go to About.</nuxt-link>
+    <nuxt-link to="/about">Navigate to About Page </nuxt-link>
   </div>
 </template>
